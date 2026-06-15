b8f5413f4fe1a96e9eb7b4ebf7ad76304f2639fc
Soham Nandi <sohamnandi012@gmail.com>
2026-06-16 01:44:34 +0530

    Update firebase.ts

--- progress_tracker/src/firebase.ts
+++ progress_tracker/src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, User } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc, collection, getDocs, deleteDoc } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAP5ZbwnQDPb990F4pH9mK7weQ8y5GuGQs",
+  authDomain: "progress-tracker-7e9cb.firebaseapp.com",
+  projectId: "progress-tracker-7e9cb",
+  storageBucket: "progress-tracker-7e9cb.firebasestorage.app",
+  messagingSenderId: "750769634352",
+  appId: "1:750769634352:web:840e047b17c44be98cc1f5",
+  measurementId: "G-HXERCNCFRR"
 };
 
 let db: any = null;
